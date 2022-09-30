f0ff599b92fd5958febbfd6b346b099f2d0e75f0
BunnyHatcher <martinengelhaus@gmx.de>
2022-09-30 23:06:34 +0200

    Implemented Footstep Sounds in Script

--- UnityFiles/Assets/Scripts/Audio/PlayerAudioManager.cs
+++ UnityFiles/Assets/Scripts/Audio/PlayerAudioManager.cs
@@ -10,15 +10,29 @@ public class PlayerAudioManager : MonoBehaviour
     public AudioClip[] swordSwing;
     public AudioClip[] attackGrunt;
 
-    [HideInInspector]
-    public AudioSource AS_SwordSwing;
-    public AudioSource AS_AttackGrunt;
+    [HideInInspector] public AudioSource AS_SwordSwing;
+    [HideInInspector] public AudioSource AS_AttackGrunt;
+
+    [Header("Footsteps")]
+    public AudioClip[] mossClips;
+    public AudioClip[] grassClips;
+    public AudioClip[] herbsClips;
+    public AudioClip[] mudClips;
+
+    [HideInInspector] public AudioSource AS_Movement;
+
+
+    private TerrainDetector terrainDetector;
+
 
     // Start is called before the first frame update
     void Start()
     {
         AS_SwordSwing = gameObject.AddComponent<AudioSource>();
         AS_AttackGrunt = gameObject.AddComponent<AudioSource>();
+
+        AS_Movement = GetComponent<AudioSource>();
+        terrainDetector = new TerrainDetector();
     }
 
     //play weapon woosh audio
@@ -34,7 +48,33 @@ public class PlayerAudioManager : MonoBehaviour
         AS_AttackGrunt.Play();
     }
 
+    // play footsteps audio
+    public void PlayFootsteps()
+    {
+        AudioClip clip = GetRandomClip();
+        AS_Movement.PlayOneShot(clip);
+    }
 
+
+    //-----------------------GET RANDOM CLIP METHOD----------------------------------------------------
+    private AudioClip GetRandomClip()
+    {
+        int terrainTextureIndex = terrainDetector.GetActiveTerrainTextureIdx(transform.position);
+
+        switch (terrainTextureIndex)
+        {
+            case 0:
+                return mossClips[Random.Range(0, mossClips.Length)];
+            case 1:
+                return grassClips[Random.Range(0, grassClips.Length)];
+            case 2:
+                return herbsClips[Random.Range(0, herbsClips.Length)];
+            case 3:
+            default:
+                return mudClips[Random.Range(0, mudClips.Length)];
+        }
+
+    }
 
 
 }
